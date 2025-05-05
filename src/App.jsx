075ebace91399a856adbcb0acca60c21bd05b3db
Furkan Arslan <furkanarslandev@gmail.com>
2025-05-05 09:32:46 +0300

    feat:add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <MainLayout />, children: [] },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+    ],
+  },
 ]);
 
 function App() {
